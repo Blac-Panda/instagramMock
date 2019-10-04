6a56bce0e9f678580b4580fcb294381033d17683
Blac-Panda <oyefesotunmise@gmail.com>
2019-10-04 16:51:51 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.flos6-1.fna.fbcdn.net/vp/cf1af7576f5c42163065119011bacf1a/5E2B1838/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net" class="rounded-circle"/>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>BabyGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>503</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">babyGram.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org </a> </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="https://instagram.flos6-1.fna.fbcdn.net/vp/5dbde3ec359ff4604efeedee795941fc/5E205718/t51.2885-15/e35/c94.0.561.561a/s150x150/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 150w,https://instagram.flos6-1.fna.fbcdn.net/vp/923e4af3d7d9e96195aa2b6c11b131e6/5E38D252/t51.2885-15/e35/c94.0.561.561a/s240x240/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 240w,https://instagram.flos6-1.fna.fbcdn.net/vp/efe20f0a4a2252b5a1150840cb5f34de/5E1FECE8/t51.2885-15/e35/c94.0.561.561a/s320x320/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 320w,https://instagram.flos6-1.fna.fbcdn.net/vp/18665edfc1acd9fb2670956dfb9dc3cb/5E3B20B2/t51.2885-15/e35/c94.0.561.561a/s480x480/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 480w,https://instagram.flos6-1.fna.fbcdn.net/vp/d08bda054030eaae8818b8546e377375/5E1CD4CF/t51.2885-15/e35/c94.0.561.561a/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 640w"/>
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.flos6-1.fna.fbcdn.net/vp/5dbde3ec359ff4604efeedee795941fc/5E205718/t51.2885-15/e35/c94.0.561.561a/s150x150/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 150w,https://instagram.flos6-1.fna.fbcdn.net/vp/923e4af3d7d9e96195aa2b6c11b131e6/5E38D252/t51.2885-15/e35/c94.0.561.561a/s240x240/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 240w,https://instagram.flos6-1.fna.fbcdn.net/vp/efe20f0a4a2252b5a1150840cb5f34de/5E1FECE8/t51.2885-15/e35/c94.0.561.561a/s320x320/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 320w,https://instagram.flos6-1.fna.fbcdn.net/vp/18665edfc1acd9fb2670956dfb9dc3cb/5E3B20B2/t51.2885-15/e35/c94.0.561.561a/s480x480/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 480w,https://instagram.flos6-1.fna.fbcdn.net/vp/d08bda054030eaae8818b8546e377375/5E1CD4CF/t51.2885-15/e35/c94.0.561.561a/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 640w"/>
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.flos6-1.fna.fbcdn.net/vp/5dbde3ec359ff4604efeedee795941fc/5E205718/t51.2885-15/e35/c94.0.561.561a/s150x150/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 150w,https://instagram.flos6-1.fna.fbcdn.net/vp/923e4af3d7d9e96195aa2b6c11b131e6/5E38D252/t51.2885-15/e35/c94.0.561.561a/s240x240/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 240w,https://instagram.flos6-1.fna.fbcdn.net/vp/efe20f0a4a2252b5a1150840cb5f34de/5E1FECE8/t51.2885-15/e35/c94.0.561.561a/s320x320/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 320w,https://instagram.flos6-1.fna.fbcdn.net/vp/18665edfc1acd9fb2670956dfb9dc3cb/5E3B20B2/t51.2885-15/e35/c94.0.561.561a/s480x480/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 480w,https://instagram.flos6-1.fna.fbcdn.net/vp/d08bda054030eaae8818b8546e377375/5E1CD4CF/t51.2885-15/e35/c94.0.561.561a/71512893_646625045864366_1532873668377478869_n.jpg?_nc_ht=instagram.flos6-1.fna.fbcdn.net&_nc_cat=102 640w"/>
+        </div>
+    </div>
+</div>
+@endsection
